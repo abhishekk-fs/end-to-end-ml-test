46894a00811f65c3acbf474edab5f1f642662f57
abhishekk-fs <abhishek.k@factspan.com>
2024-03-13 13:45:00 +0530

    updated exception and utils file

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
 from us_visa.logger import logging
+from us_visa.exception import USVisaException
+import sys
 
-logging.info("Welcome to our custom log")
+try:
+    a = 1 / "10"
+except Exception as e:
+    raise USVisaException(e, sys) from e
