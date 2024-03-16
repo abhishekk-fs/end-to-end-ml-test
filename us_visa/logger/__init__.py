9c97183721e845d6b98d8804542b45910de6e445
Abhishek Kumar <abhishek.k@factspan.com>
2024-03-16 12:54:54 +0530

    updated

--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -6,11 +6,12 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_dir = 'logs'
+log_dir = "logs"
 
 logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
 
-os.makedirs(logs_path, exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
+
 
 logging.basicConfig(
     filename=logs_path,
